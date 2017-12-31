9b6a01176f873414a86c079bf641e2d7b403fe40
Fredbob396 <fredbob396@gmail.com>
2017-12-31 18:31:10 -0500

    Added spaces for results

--- Morrowind Enchantment Simulator/Form1.Designer.cs
+++ Morrowind Enchantment Simulator/Form1.Designer.cs
@@ -63,6 +63,13 @@
             this.effectCostMultBox = new System.Windows.Forms.TextBox();
             this.typeBox = new System.Windows.Forms.ComboBox();
             this.resetButton = new System.Windows.Forms.Button();
+            this.verticalDivider = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // enchantBox
@@ -379,11 +386,80 @@
             this.resetButton.UseVisualStyleBackColor = true;
             this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
+            // verticalDivider
+            // 
+            this.verticalDivider.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.verticalDivider.Location = new System.Drawing.Point(402, -30);
+            this.verticalDivider.Name = "verticalDivider";
+            this.verticalDivider.Size = new System.Drawing.Size(2, 339);
+            this.verticalDivider.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(410, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Enchantment Points";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(410, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(410, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Cast Cost";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(410, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(410, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Craft Chance";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(410, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 38;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 505);
+            this.ClientSize = new System.Drawing.Size(543, 301);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.verticalDivider);
             this.Controls.Add(this.resetButton);
             this.Controls.Add(this.typeBox);
             this.Controls.Add(this.effectCostMultBox);
@@ -418,6 +494,7 @@
             this.Controls.Add(this.luckBox);
             this.Controls.Add(this.intelligenceBox);
             this.Controls.Add(this.enchantBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Morrowind Enchantment Simulator";
@@ -463,6 +540,13 @@
         private System.Windows.Forms.TextBox effectCostMultBox;
         private System.Windows.Forms.ComboBox typeBox;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label verticalDivider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
